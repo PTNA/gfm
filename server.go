76c03f0fae18e30f71f0de5aab54c0c7ea9c59df
Benjamin Nørgaard <b@noergaard.dk>
2016-10-25 14:43:29 +0200

    Implement dbnames command

--- server.go
+++ server.go
@@ -1,9 +1,17 @@
 package gfm
 
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
 // Server is the interface of a FileMaker server
 type Server interface {
 	DB(string) Database
-	DBs() []Database
+	DBs() ([]Database, error)
+	URL() string
 }
 
 type server struct {
@@ -21,8 +29,44 @@ func (s server) DB(name string) Database {
 	return database{server: s, name: name}
 }
 
-func (s server) DBs() []Database {
+func (s server) DBs() ([]Database, error) {
 	var dbs []Database
-	// TODO: Implement this using -dbnames
-	return dbs
+
+	var b bytes.Buffer
+	b.WriteString(s.URL())
+	b.WriteString("-dbnames")
+	res, err := http.Get(b.String())
+
+	if err != nil {
+		// TODO: maybe we need a better error here
+		return dbs, err
+	}
+
+	var fmrs FMResultSet
+	err = xml.NewDecoder(res.Body).Decode(&fmrs)
+	if err != nil {
+		return dbs, err
+	} else if fmrs.Error.Code != 0 {
+		return dbs, fmt.Errorf("FMError: %v", fmrs.Error.Code)
+	}
+
+	for _, r := range fmrs.ResultSet.Records {
+		name := r.Fields[0].Data
+		db := database{server: s, name: name}
+		dbs = append(dbs, db)
+	}
+
+	return dbs, nil
+}
+
+func (s server) URL() string {
+	var b bytes.Buffer
+	b.WriteString("http://")
+	b.WriteString(s.user)
+	b.WriteString(":")
+	b.WriteString(s.password)
+	b.WriteString("@")
+	b.WriteString(s.host)
+	b.WriteString("/fmi/xml/fmresultset.xml?")
+	return b.String()
 }
